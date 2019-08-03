21c4f606e40e66de567ef1be347fbd60fdd97f39
Rijnard van Tonder <rvantonder@gmail.com>
2019-08-03 01:04:20 -0400

    pull out functions

--- src/frontend/query_commands.ml
+++ src/frontend/query_commands.ml
@@ -796,6 +796,30 @@ let rec dispatch : type a. Mpipeline.t -> a Query_protocol.t -> a =
     let raw_source = Mpipeline.raw_source pipeline in
     let source = raw_source |> Msource.text in
     let line_ranges = line_ranges source in
+    let get_type_info_at_range range =
+      (* START: Copy-pasta query_json *)
+      let with_location ?(skip_none=false) loc assoc =
+        if skip_none && loc = Location.none then
+          `Assoc assoc
+        else
+          `Assoc (("start", Lexing.json_of_position loc.Location.loc_start) ::
+                  ("end",   Lexing.json_of_position loc.Location.loc_end) ::
+                  assoc)
+      in
+      let json_of_type_loc (loc,desc,_) =
+        with_location loc [
+          "type", (match desc with
+              | `String _ as str -> str
+              | `Index n -> `Int n)
+        ]
+      in
+      (* END: Copy-pasta query_json *)
+      let results = dispatch pipeline (Query_protocol.Type_enclosing (None,range,Some 0)) in
+      (* Take the first result *)
+      match results with
+      | hd :: _ -> Some (json_of_type_loc hd)
+      | _ -> None
+    in
     let entries : String.Set.t =
       Core.List.foldi line_ranges ~init:String.Set.empty ~f:(fun line acc character_ranges ->
           Format.eprintf "%2.0f%%%!" ((Core.Int.to_float line) /. (Core.Int.to_float (List.length line_ranges)) *. 100.0);
@@ -805,32 +829,7 @@ let rec dispatch : type a. Mpipeline.t -> a Query_protocol.t -> a =
               let range = `Logical (line+1, character) in
               (*let enclosing = dispatch pipeline (Query_protocol.Enclosing range) in
                 let json = dispatch pipeline (Query_protocol.Locate (prefix,lookfor,range)) in*)
-              let type_result =
-                let results =
-                  dispatch pipeline (Query_protocol.Type_enclosing (None,range,Some 0))
-                in
-                (* START: Copy-pasta query_json *)
-                let with_location ?(skip_none=false) loc assoc =
-                  if skip_none && loc = Location.none then
-                    `Assoc assoc
-                  else
-                    `Assoc (("start", Lexing.json_of_position loc.Location.loc_start) ::
-                            ("end",   Lexing.json_of_position loc.Location.loc_end) ::
-                            assoc)
-                in
-                let json_of_type_loc (loc,desc,_) =
-                  with_location loc [
-                    "type", (match desc with
-                        | `String _ as str -> str
-                        | `Index n -> `Int n)
-                  ]
-                in
-                (* END: Copy-pasta query_json *)
-                (* Take the first result *)
-                match results with
-                | hd :: _ -> Some (json_of_type_loc hd)
-                | _ -> None
-              in
+              let type_result = get_type_info_at_range range in
               match type_result with
               | Some json ->
                 let stringified_json = Std.Json.to_string json in
